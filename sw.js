8bfd35ff5078dca2f28e40d2d209e83efcf463da
Leeak82 <166353240+Leeak82@users.noreply.github.com>
2026-05-05 02:05:32 -0700

    Add offline service worker

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'value-scanner-v1';
+const FILES = ['./', './index.html', './style.css', './app.js', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+});
